6bc2342f6cf4e495ed4318661239d73a1a41d78f
mansurbek046 <mansurbek046@gmail.com>
2024-01-14 10:39:21 +0000

    requirements list added

--- jwtapp/urls.py
+++ jwtapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns=[
 	path('users/', Register.as_view({'post':'create'}), name='user-create'),
-	path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair')
+	path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/refresh', TokenRefreshView.as_view(), name='token-refresh')
 ]
